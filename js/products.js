cb44465ecbe464711072688a4e41d38e8f280d27
Malena <malechiesa24@gmail.com>
2021-08-22 10:52:40 -0300

    input kcyo

--- js/products.js
+++ js/products.js
@@ -1,6 +1,67 @@
+
+var categoriesArray = [];
+
+function showProductsList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let category = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.name +`</h4> 
+                        <p> `+ category.description +`</p> 
+                        <p> `+ category.cost +`<p>
+                        </div>
+                        <small class="text-muted">` + category.soldCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML
+    }
+}
+
+/// function cargarDatos(productos_url) {
+///       document.getElementById("data").innerHTML = "";
+///        fetch(productos_url)
+///            .then(respuesta => respuesta.json())
+///            .then(datos =>{
+
+
+///            for (let i=0; <datos.length; i++){
+///                let row = "";
+///                row =`
+///                    <tr>
+///                        <td>` + datos[i].nombre + `</td>
+///                        <td>` + datos[i].categoria + `</td>
+///                        <td>` + datos[i].origen + `</td>
+///                    </tr>`;
+///                
+///                document.getElementById("data").innerHTML += row;
+///            
+///
+///            })
+///            .catch(error => alert("Hubo un error: "+ error ));
+///    }
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            showProductsList(categoriesArray);
+        }
+    });
 });
